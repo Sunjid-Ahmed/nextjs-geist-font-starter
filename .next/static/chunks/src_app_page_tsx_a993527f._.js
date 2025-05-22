a7cc5dd3b2a0e3920e941cbce00009f968fa3599
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-05-22 18:10:16 +0600

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747915814126

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2547e221._.js",
+  "static/chunks/src_04e582be._.js"
+],
+    source: "dynamic"
+});
